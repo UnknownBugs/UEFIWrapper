3cdbc6a93456391f534861600bbc400a02f34d8c
sunrisepeak <speakshen@163.com>
2021-12-17 19:33:45 +0800

    add io wrapper

---
+++ core/wrapper/io_wrapper.hpp
@@ -0,0 +1,22 @@
+#ifndef __IO_WRAPPER_HPP__
+#define __IO_WRAPPER_HPP__
+
+namespace UEFIWrapper {
+namespace IO {
+
+class StdIn {
+
+};  // StdIn
+
+class StdOut {
+
+};  // StdOut
+
+class File {
+
+};  // File
+};  // namespace IO
+
+}; // namespace UEFIWrapper
+
+#endif // __IO_WRAPPER_HPP__
